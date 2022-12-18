23389e69460e39a700541e357000e48d05bcb35c
Sergey Kucherin <skucherin@yandex.ru>
2022-12-18 19:11:24 +0700

    Example 005 Condition If-Else added

---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "ириша")
+{
+    Console.WriteLine("Ура, это же ИРИША!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.Write(username);
+}
