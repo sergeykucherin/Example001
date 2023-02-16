8565c1982f4172d6cf66b19169876f0ad909f54a
Sergey Kucherin <skucherin@yandex.ru>
2023-02-17 02:28:07 +0700

    Max array number is found using function

---
+++ Example009_IntroArray/Program.cs
@@ -0,0 +1,19 @@
+﻿// Определение максимального числа из массива с помощью функции
+using static System.Console;
+Clear();
+
+int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if(arg2 > result) result = arg2;
+    if(arg3 > result) result = arg3;
+    return result;
+}
+//              0   1   2   3   4   5   6   7   8
+int[] array = {19, 63, 54, 17, 127, 85, 32, 15, 21};
+
+
+
+int max = Max(Max(array[0], array[1], array[2]), Max(array[3], array[4], array[5]), Max(array[6], array[7], array[8]));
+
+Write($"max = {max} ");
