8baf1c3e6bb95a469d49b8ed81ef802968a62afe
Sergey Kucherin <skucherin@yandex.ru>
2023-02-17 03:54:16 +0700

    Searched array number found

---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,20 @@
+﻿using static System.Console;
+Clear();
+
+int[] array = {1, 5, 24, 19, 15, 32, 11, 24};
+
+int n = array.Length;
+int find = 24;
+
+int index = 0;
+
+while (index < n)
+{
+    if(array[index] == find)
+    {
+        WriteLine(index);
+        break;
+    }
+
+index++;    
+}
