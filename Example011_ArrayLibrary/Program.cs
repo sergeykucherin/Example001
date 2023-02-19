272d0c5e1dccaaea53ee2887ef015aa8bf4fbe7a
Sergey Kucherin <skucherin@yandex.ru>
2023-02-19 19:18:25 +0700

    Added solution Example011 correct

--- Example011_ArrayLibrary/Program.cs
+++ Example011_ArrayLibrary/Program.cs
@@ -1,4 +1,4 @@
-﻿// Поиск элемента массива, созданного генератором случайных чисел, с помощью метода
+﻿// Поиск элемента массива, созданного генератором случайных чисел, с помощью метода (если позиция не найдена, то отобразиться -1)
 
 using static System.Console;
 Clear();
@@ -19,14 +19,38 @@ void PrintArray(int[] col)
     int position = 0;
     while (position < count)
     {
-        WriteLine(col[position]);
+        WriteLine(col[position] );
         position++;
     }
+}
+int IndexOf(int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = -1;
+    while (index < count)
+    {
+        if (collection[index] == find)
+        {
+            position = index;
+            break;
+        }
+        index++;
+    }
+    return position;
 }
 
 int[] array = new int[10];
 FillArray(array);
+array[4] = 5;
+array[6] = 5;
 PrintArray(array);
+WriteLine();
+
+int pos = IndexOf(array, 444);
+WriteLine(pos);
+
+
 
 
 
