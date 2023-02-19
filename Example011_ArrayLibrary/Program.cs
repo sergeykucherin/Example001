eb62c12821d1c9d38a5959b4ebcbd7e8bc02751f
Sergey Kucherin <skucherin@yandex.ru>
2023-02-19 19:13:33 +0700

    Added solution Example011

--- Example011_ArrayLibrary/Program.cs
+++ Example011_ArrayLibrary/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Поиск элемента массива, созданного генератором случайных чисел, с помощью метода
+
+using static System.Console;
+Clear();
+
+void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random() .Next(1, 10);
+        index++;
+    }
+}
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        WriteLine(col[position]);
+        position++;
+    }
+}
+
+int[] array = new int[10];
+FillArray(array);
+PrintArray(array);
+
+
+
+
+
+
